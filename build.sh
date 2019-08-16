14b51fa88c4144641665f52ac3e7b7b39c7ce158
Oriol Teixidó <oriol.teixido@gmail.com>
2019-08-16 07:45:49 +0200

    WIP. Build all the architectures in one DockerHub build

--- build.sh
+++ build.sh
@@ -13,33 +13,21 @@ trap Cleanup 1 2 3 6
 
 Cleanup() {
     if [ ! -z "$WORK_DIR" ]; then
-        rm -rf "$WORK_DIR/plugin/certbot" || true
-        rm -rf "$WORK_DIR/core/certbot" || true
+        rm -rf "$WORK_DIR"/core/qemu-*-static || true
+        rm -rf "$WORK_DIR"/plugin/qemu-*-static || true
     fi
     popd 2> /dev/null || true
-}
-
-BuildArch() {
-    DOCKER_ARCH="$1"
-    DOCKER_REPO="$2"
-    CONTEXT_PATH="$3"
-    DOCKERFILE_PATH="$CONTEXT_PATH/Dockerfile"
-    DOCKER_TAG="$DOCKER_ARCH-$CERTBOT_VERSION"
-
-    pushd "$CONTEXT_PATH"
-        DOCKER_TAG="$DOCKER_TAG" DOCKER_REPO="$DOCKER_REPO" DOCKERFILE_PATH="$DOCKERFILE_PATH" bash hooks/pre_build
-        DOCKER_TAG="$DOCKER_TAG" DOCKER_REPO="$DOCKER_REPO" DOCKERFILE_PATH="$DOCKERFILE_PATH" bash hooks/build
-        DOCKER_TAG="$DOCKER_TAG" DOCKER_REPO="$DOCKER_REPO" DOCKERFILE_PATH="$DOCKERFILE_PATH" bash hooks/post_build
-    popd
 }
 
 Build() {
     DOCKER_REPO="$1"
     CONTEXT_PATH="$2"
-    for DOCKER_ARCH in amd64 arm32v6; do
-        Cleanup
-        BuildArch "${DOCKER_ARCH}" "${DOCKER_REPO}" "${CONTEXT_PATH}"
-    done
+    DOCKERFILE_PATH="$CONTEXT_PATH/Dockerfile"
+    DOCKER_TAG="$CERTBOT_VERSION"
+    pushd "$CONTEXT_PATH"
+        DOCKER_TAG="$DOCKER_TAG" DOCKER_REPO="$DOCKER_REPO" DOCKERFILE_PATH="$DOCKERFILE_PATH" bash hooks/pre_build
+        DOCKER_TAG="$DOCKER_TAG" DOCKER_REPO="$DOCKER_REPO" DOCKERFILE_PATH="$DOCKERFILE_PATH" bash hooks/build
+    popd
 }
 
 WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
@@ -67,7 +55,6 @@ CERTBOT_PLUGINS_DOCKER_REPOS=(
     "certbot/dns-sakuracloud"
 )
 
-sed -i -E "s|FROM .+|FROM certbot/certbot:\${TARGET_ARCH}-$CERTBOT_VERSION|g" "$WORK_DIR"/plugin/Dockerfile
 for DOCKER_REPO in "${CERTBOT_PLUGINS_DOCKER_REPOS[@]}"; do
     Build "${DOCKER_REPO}" "$WORK_DIR/plugin"
 done
